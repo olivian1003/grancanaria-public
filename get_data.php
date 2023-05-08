9b206570a338685784a8f9cba7a8d764c5d23c5b
olivian1003 <131756774+olivian1003@users.noreply.github.com>
2023-05-08 10:16:31 +0100

    DA

--- get_data.php
+++ get_data.php
@@ -3,6 +3,11 @@
 <h1>data</h1>
 <?php
 echo "<br> The name of the place is ".$_POST["fname"];
+echo "<br> The name of the place is ".$_POST["fname"];
+echo "<br> The name of the place is ".$_POST["fname"];
+echo "<br> The name of the place is ".$_POST["fname"];
+echo "<br> The name of the place is ".$_POST["fname"];
+echo "<br> The name of the place is ".$_POST["fname"];
 ?>
 
 
