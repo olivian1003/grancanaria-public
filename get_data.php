68cb4103166edd4573f2c09396e5059271f2f132
olivian1003 <131756774+olivian1003@users.noreply.github.com>
2023-05-08 10:28:59 +0100

    da

--- get_data.php
+++ get_data.php
@@ -6,7 +6,7 @@ echo "<br> The name of the place is ".$_POST["fname"];
 echo "<br> The description of the place is ".$_POST["Description"];
 echo "<br> The Municipio is ".$_POST["Municipio"];
 echo "<br> The Url of the place is ".$_POST["Url"];
-echo "<br> The Url google maps of the place is ".$_POST["Url google maps"];
+echo "<br> The Url google maps of the place is ".$_POST["Url-info"];
 echo "<br> The name of the place is ".$_POST["filename"];
 ?>
 
