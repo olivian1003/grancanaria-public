4ac7930b5f7881ab02a8a0de13bbbf2ac6970fd1
olivian1003 <131756774+olivian1003@users.noreply.github.com>
2023-05-08 10:24:41 +0100

    da

--- get_data.php
+++ get_data.php
@@ -4,7 +4,7 @@
 <?php
 echo "<br> The name of the place is ".$_POST["fname"];
 echo "<br> The description of the place is ".$_POST["Description"];
-echo "<br> The name of the place is ".$_POST["Municipio"];
+echo "<br> The Municipio is ".$_POST["Municipio"];
 echo "<br> The name of the place is ".$_POST["fname"];
 echo "<br> The name of the place is ".$_POST["fname"];
 echo "<br> The name of the place is ".$_POST["fname"];
