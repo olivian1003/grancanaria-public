01f25b30e5612dc3166485cf8cd644951999de06
olivian1003 <131756774+olivian1003@users.noreply.github.com>
2023-05-08 10:05:44 +0100

    da

--- hello.php
+++ hello.php
@@ -1,5 +1,23 @@
+<html>
+<body>
+<h1>ce faci azi?</h1>
+
+
+
 <?php
 
-echo "hello";
+$model = "cool";
+echo "My cap is " . $model . "<br>";
+$x=5;
+$y=4;
+$result= $x+$y;
+echo "result: $result";
+print "<h2>holaaaaa</h2>";
+
+
+
 
 ?>
+
+</body>
+</html>
