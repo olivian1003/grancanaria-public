0c9e82dbc7d53256476feb951569e82b7e73d853
olivian1003 <131756774+olivian1003@users.noreply.github.com>
2023-05-09 10:05:58 +0100

    da

--- get_data.php
+++ get_data.php
@@ -8,6 +8,13 @@ echo "<br> The Municipio is ".$_POST["Municipio"];
 echo "<br> The official site of the place is ".$_POST["Url"];
 echo "<br> The location on google maps of the place is ".$_POST["maps"];
 echo "<br> Attached file: ".$_POST["filename"];
+
+if($_POST["fname"] == ""){
+    echo "<br>ERROR: the field is empty";
+}
+else{
+    echo "<br>ERROR: the field is empty";
+}
 ?>
 
 
