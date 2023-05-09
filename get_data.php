acd75c22218af9174bcab4eea447d0fa7eca9270
olivian1003 <131756774+olivian1003@users.noreply.github.com>
2023-05-09 10:28:58 +0100

    da

--- get_data.php
+++ get_data.php
@@ -3,7 +3,7 @@
 <h1>data</h1>
 <?php
 //echo "<br> The name of the place is ".$_POST["fname"];
-echo "<br> The description of the place is ".$_POST["Description"];
+//echo "<br> The description of the place is ".$_POST["Description"];
 echo "<br> The Municipio is ".$_POST["Municipio"];
 echo "<br> The official site of the place is ".$_POST["Url"];
 echo "<br> The location on google maps of the place is ".$_POST["maps"];
@@ -20,6 +20,7 @@ else{
     echo "<br>The name of the place is ".$_POST["fname"];
 }
 }
+
 if($_POST["Description"] == ""){
     echo "<br>ERROR: the field is empty";
 }
@@ -29,6 +30,18 @@ else{
     }
     else{
     echo "<br>The Description of the place is ".$_POST["Description"];
+}
+}
+
+if($_POST["Municipio"] == ""){
+    echo "<br>ERROR: the field is empty";
+}
+else{
+    if((strlen($_POST["Municipio"])<100)){
+        echo "<br>ERROR: the lenght is too big";
+    }
+    else{
+    echo "<br>The Municipio is ".$_POST["Municipio"];
 }
 }
 
