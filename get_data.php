efa7e0a889d190a983358848de7773caccbe29de
olivian1003 <131756774+olivian1003@users.noreply.github.com>
2023-05-09 10:16:37 +0100

    da

--- get_data.php
+++ get_data.php
@@ -14,13 +14,34 @@ if($_POST["fname"] == ""){
 }
 else{
     echo "<br>The name of the place is ".$_POST["fname"];
-}
+
 if((strlen($_POST["fname"])>10)){
     echo "<br>ERROR: the lenght is too big";
 }
 else{
     echo "<br>The name of the place is ".$_POST["fname"];
 }
+}
+
+if($_POST["Description"] == ""){
+    echo "<br>ERROR: the field is empty";
+}
+else{
+    echo "<br>TThe description of the place is ".$_POST["Description"];
+}
+if((strlen($_POST["Description"])>100)){
+    echo "<br>ERROR: the lenght is too big";
+}
+else{
+    echo "<br>The description of the place is ".$_POST["Description"];
+}
+
+
+
+
+
+
+
 ?>
 
 
