e6d781d40bf4a5f5edfa366bd58e057d91937a6b
olivian1003 <131756774+olivian1003@users.noreply.github.com>
2023-05-09 10:29:28 +0100

    da

--- get_data.php
+++ get_data.php
@@ -4,10 +4,10 @@
 <?php
 //echo "<br> The name of the place is ".$_POST["fname"];
 //echo "<br> The description of the place is ".$_POST["Description"];
-echo "<br> The Municipio is ".$_POST["Municipio"];
-echo "<br> The official site of the place is ".$_POST["Url"];
-echo "<br> The location on google maps of the place is ".$_POST["maps"];
-echo "<br> Attached file: ".$_POST["filename"];
+//echo "<br> The Municipio is ".$_POST["Municipio"];
+//echo "<br> The official site of the place is ".$_POST["Url"];
+//echo "<br> The location on google maps of the place is ".$_POST["maps"];
+//echo "<br> Attached file: ".$_POST["filename"];
 
 if($_POST["fname"] == ""){
     echo "<br>ERROR: the field is empty";
