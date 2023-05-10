306495b99f04d372653b73065ff6d5fe9f14c4b4
olivian1003 <131756774+olivian1003@users.noreply.github.com>
2023-05-10 10:01:00 +0100

    da

--- get_data.php
+++ get_data.php
@@ -8,7 +8,7 @@ $txt = "";
 fwrite($myfile, $txt);
 $txt = "";
 fwrite($myfile, $txt);
-fwrite($myfile, $fname);
+fwrite($myfile, $_POST["fname"]);
 fwrite($myfile, $Description);
 fwrite($myfile, $Municipio);
 fwrite($myfile, $Url);
