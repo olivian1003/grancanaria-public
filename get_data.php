b3a361779a4cff17ce96ad718db9be7c24e0742b
olivian1003 <131756774+olivian1003@users.noreply.github.com>
2023-05-10 11:16:37 +0100

    da

--- get_data.php
+++ get_data.php
@@ -19,7 +19,7 @@ fwrite($myfile,"\r\n");
 fwrite($myfile, $_POST["maps"]);
 fwrite($myfile,"\r\n");
 fwrite($myfile, $_POST["filename"]);
-fwrite($myfile,"<br>");
+fwrite($myfile,"");
  fclose($myfile);
  
 
