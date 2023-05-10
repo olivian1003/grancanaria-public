51d16ad38ccc1499149fe0c1971a5d0feadba987
olivian1003 <131756774+olivian1003@users.noreply.github.com>
2023-05-10 08:54:20 +0100

    da

--- get_data.php
+++ get_data.php
@@ -8,7 +8,6 @@
 //echo "<br> The official site of the place is ".$_POST["Url"];
 //echo "<br> The location on google maps of the place is ".$_POST["maps"];
 //echo "<br> Attached file: ".$_POST["filename"];
-
 if($_POST["fname"] == ""){
     echo "<br>ERROR: the field is empty";
 }
