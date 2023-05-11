d4a7f40c5dec7814749616451d1a65525490de21
olivian1003 <131756774+olivian1003@users.noreply.github.com>
2023-05-11 11:26:57 +0100

    da

--- test.php
+++ test.php
@@ -1,16 +1,13 @@
 <?php
-echo "<table border='1'><tr><th>Nombre</th><th>Descripción</th></tr><tr><th>Municipio</th><th>Descripción</th></tr>";
+      echo "<table border='1'><tr><th>Nombre</th><th>Descripción</th><th>Descripción</th><th>URL con más información</th><th>URL de Google Maps</th><th>Imagen</th>";
+       $myfile = fopen("file.csv","r") or die("Unable to open file!");
+       while(!feof($myfile)){
+           $line = fgets(($myfile));
 
+           $array_line = explode(";",$line);
+           echo "<tr><td>".$array_line[0]."</td><td>"."".$array_line[1]."</td><td>".$array_line[2].""."</td><td>".$array_line[3].""."</td><td>".$array_line[4].""."</td><td>".$array_line[5];
+       }
+      fclose($myfile);
 
-$myfile = fopen("file.csv", "r") or die("Unable to open file!");
-while(!feof($myfile)) {
-    $line = fgets($myfile);
-     $array_line = explode(";",$line);  
-    echo "<tr><td>".$array_line[0]."</td><td>".$array_line[1]."</td></tr>";
-}
-fclose($myfile);
-echo "</table>"
-
-
-
+       echo "</table>";
 ?>
