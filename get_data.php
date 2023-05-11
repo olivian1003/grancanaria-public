b8ba9ffbb42e13c416bbe9fc1b0bb876f4cfe772
olivian1003 <131756774+olivian1003@users.noreply.github.com>
2023-05-11 10:38:29 +0100

    da

--- get_data.php
+++ get_data.php
@@ -107,19 +107,6 @@ if ($error == false) {
     fwrite($myfile,"; \n");
     fclose($myfile);
 }
-
-
-echo "<html><body><table>\n\n";
-$f = fopen("file.csv", "r");
-while (($line = fgetcsv($f)) !== false) {
-        echo "<tr>";
-        foreach ($line as $cell) {
-                echo "<td>" . htmlspecialchars($cell) . "</td>";
-        }
-        echo "</tr>\n";
-}
-fclose($f);
-echo "\n</table></body></html>";
 ?>
 
 
---
+++ test.php
@@ -0,0 +1,20 @@
+<?php
+echo "<table border='1'><tr><th>Nombre</th><th>Descripción</th></tr>";
+
+
+$myfile = fopen("places.csv", "r") or die("Unable to open file!");
+while(!feof($myfile)) {
+    $line = fgets($myfile);
+     $array_line = explode(";",$line);  
+    echo "<tr><td>".$array_line[0]."</td><td>".$array_line[1]."</td></tr>";
+}
+fclose($myfile);
+
+echo "</table>";
+
+fclose($myfile);
+echo "</table>"
+
+
+
+?>
