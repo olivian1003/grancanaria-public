81c14240b12b1488fb0eb4dede5af2f04d5dd7e7
olivian1003 <131756774+olivian1003@users.noreply.github.com>
2023-05-15 13:07:00 +0100

    da

--- get_data.php
+++ get_data.php
@@ -3,13 +3,6 @@
 <h1>data</h1>
 
 <?php
-//echo "<br> The name of the place is ".$_POST["fname"];
-//echo "<br> The description of the place is ".$_POST["Description"];
-//echo "<br> The Municipio is ".$_POST["Municipio"];
-//echo "<br> The official site of the place is ".$_POST["Url"];
-//echo "<br> The location on google maps of the place is ".$_POST["maps"];
-//echo "<br> Attached file: ".$_POST["filename"];
-
 
 $error = false;
 
