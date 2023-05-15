36c430ff490d6077bce6d8ece3a5f0d064f51089
olivian1003 <oliviacojocia@alumno.ieselrincon.es>
2023-05-15 10:20:35 +0100

    da

--- test.php
+++ test.php
@@ -1,15 +1,69 @@
 
-
-
 <?php
-      echo "<table border='1'><tr><th>Nombre</th><th>Descripción</th><th>Descripción</th><th>URL con más información</th><th>URL de Google Maps</th><th>Imagen</th></tr>";
-       $myfile = fopen("file.csv","r") or die("Unable to open file!");
-       while(!feof($myfile)){
-           $line = fgets(($myfile));
-           $array_line = explode(";",$line);
-           echo "<tr><td>".$array_line[0]."</td><td>"."".$array_line[1]."</td><td>".$array_line[2].""."</td><td>".$array_line[3].""."</td><td>".$array_line[4].""."</td><td>".$array_line[5];
-       }
-      fclose($myfile);
+$file = fopen("file.csv", "r"); 
+$places = array();
+while(!feof($file)) {
+    $places[] = explode(";",fgets($file));
+}
 
-       echo "</table>";
+
+
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Descubriendo Gran Canaria</title>
+</head>
+<body>
+	<header>
+		<h1>Descubriendo Gran Canaria</h1>
+	</header>
+	<nav>
+		<ul>
+			<li><a href="nuevo.html">Nuevo lugar que visitar</a></li>
+			<li><a href="listado.php">Lugares que no me puedo perder</a></li>
+		</ul>
+	</nav>
+    <br>
+	<main>
+        <div class="containerListado">
+            <h1>Lugares que no  me puedo perder</h1>
+            <table border="1">
+                <thead>
+                    <tr>
+                        <th>Visitado</th>
+                        <th>Nombre del lugar</th>
+                        <th>Descripción</th>
+                        <th>Municipio</th>
+                        <th>URL con más información</th>
+                        <th>URL de Google Maps</th>
+                        <th>Imagen</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                    <?php
+
+                       foreach ($places as $place) {
+                            
+                            echo "<tr>";
+                            echo "<td><input type='checkbox' checked='checked'></td>";
+                            echo "<td>$place[0]</td>";
+                            echo "<td>$place[1]</td>";
+                            echo "<td>$place[2]</td>";
+                            echo "<td><a href='$place[3]'>$place[3]</a></td>";
+                            echo "<td><a href='$place[4]'>$place[4]</a></td>";
+                            echo "<td><img src='upload/".$place[5]."' width='200px'></td>";
+                            echo "</tr>";
+
+                        }
+
+                    ?>
+                
+                </tbody>
+            </table>
+        </div>
+    </main>
+</body>
+</html>    
