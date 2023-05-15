84d40f4cc016556079d9a04b65fa3d81cf45f2ce
olivian1003 <131756774+olivian1003@users.noreply.github.com>
2023-05-15 12:44:54 +0100

    da

--- get_data.php
+++ get_data.php
@@ -109,7 +109,7 @@ if ($error == false) {
     fwrite($myfile, $_POST["maps"]);
     fwrite($myfile,";");
     fwrite($myfile, $_FILES["filename"]["name"]);
-    fwrite($myfile, ";\n");
+    fwrite($myfile, $txt);
     fclose($myfile);
 }
 ?>
