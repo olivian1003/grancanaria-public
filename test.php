ece7f90a692d97b6693b3f47d4148def228bb6c8
olivian1003 <oliviacojocia@alumno.ieselrincon.es>
2023-05-15 09:10:29 +0100

    da

--- test.php
+++ test.php
@@ -1,8 +1,11 @@
+
+
+
 <?php
       echo "<table border='1'><tr><th>Nombre</th><th>Descripción</th><th>Descripción</th><th>URL con más información</th><th>URL de Google Maps</th><th>Imagen</th></tr>";
        $myfile = fopen("file.csv","r") or die("Unable to open file!");
        while(!feof($myfile)){
-           $line = fgets($myfile);
+           $line = fgets(($myfile));
            $array_line = explode(";",$line);
            echo "<tr><td>".$array_line[0]."</td><td>"."".$array_line[1]."</td><td>".$array_line[2].""."</td><td>".$array_line[3].""."</td><td>".$array_line[4].""."</td><td>".$array_line[5];
        }
